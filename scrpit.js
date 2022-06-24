2c3d3138761dc1d155c89a7bacccac2e51fea038
Sushant Sharma <ssushantlb08@gmail.com>
2022-06-24 23:50:32 +0530

    functionality: buttons

--- scrpit.js
+++ scrpit.js
@@ -2,9 +2,10 @@ const newTaskBTN = document.querySelector(".submitBtn");
 const container = document.querySelector(".task");
 const newTodoItem = document.getElementById("newTodo");
 
-newTaskBTN.addEventListener("click" , function() {
+newTaskBTN.addEventListener("click", function () {
     var input = document.createElement("input");
     input.type = "text";
+    input.setAttribute('readonly', '');
     input.value = newTodoItem.value;
 
     var editBTN = document.createElement("button");
@@ -18,11 +19,40 @@ newTaskBTN.addEventListener("click" , function() {
     var doneBTN = document.createElement("button");
     doneBTN.textContent = "Done";
     doneBTN.className = "Btn";
-container.appendChild(input);
-container.appendChild(editBTN);
-container.appendChild(doneBTN);
-container.appendChild(deleteBTN);
-input.setAttribute('readonly','');
-event.preventDefault();
+
+    container.appendChild(input);
+    container.appendChild(editBTN);
+    container.appendChild(doneBTN);
+    container.appendChild(deleteBTN);
+    event.preventDefault();
+
+    editBTN.addEventListener("click", function(){
+        input.removeAttribute("readonly", "");
+        if (editBTN.textContent==="Edit") {
+            editBTN.textContent = "Save";
+        } else {
+            editBTN.textContent = "Edit";
+            input.setAttribute("readonly", "");
+        }
+    })
+    
+    deleteBTN.addEventListener("click" , function(){
+        input.remove();
+        editBTN.remove();
+        deleteBTN.remove();
+        doneBTN.remove();
+    })
+    
+    doneBTN.addEventListener("click" , function(){
+        input.style.backgroundColor="rgb(151, 240, 151)";
+        input.style.color = "black";
+        setTimeout(() => {
+            input.remove();
+            editBTN.remove();
+            deleteBTN.remove();
+            doneBTN.remove();
+        }, 1000);
+    })
+
 }
 )
