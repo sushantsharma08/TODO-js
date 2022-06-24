6e2fcc6f4ff696605556d41ac165252affc874aa
Sushant Sharma <ssushantlb08@gmail.com>
2022-06-25 01:04:26 +0530

    update : front end done

--- scrpit.js
+++ scrpit.js
@@ -1,8 +1,16 @@
 const newTaskBTN = document.querySelector(".submitBtn");
 const container = document.querySelector(".task");
 const newTodoItem = document.getElementById("newTodo");
+const buttons_div = document.querySelector(".buttons");
+const tasksDone = document.querySelector(".TaskCount");
+let counter = 0;
 
 newTaskBTN.addEventListener("click", function () {
+    var div = document.createElement("div");
+    div.className = "taskdiv";
+
+    var buttons_div = document.createElement("div");
+
     var input = document.createElement("input");
     input.type = "text";
     input.setAttribute('readonly', '');
@@ -20,37 +28,55 @@ newTaskBTN.addEventListener("click", function () {
     doneBTN.textContent = "Done";
     doneBTN.className = "Btn";
 
-    container.appendChild(input);
-    container.appendChild(editBTN);
-    container.appendChild(doneBTN);
-    container.appendChild(deleteBTN);
+    if (!input.value) {
+        alert("please enter a value");
+        return;
+    }
+
+
+    div.appendChild(buttons_div);
+    container.appendChild(div);
+    div.appendChild(input);
+    buttons_div.appendChild(editBTN);
+    buttons_div.appendChild(doneBTN);
+    buttons_div.appendChild(deleteBTN);
+
     event.preventDefault();
 
-    editBTN.addEventListener("click", function(){
+    editBTN.addEventListener("click", function () {
         input.removeAttribute("readonly", "");
-        if (editBTN.textContent==="Edit") {
+        if (editBTN.textContent === "Edit") {
+            input.style.color = "rgb(193, 100, 202)";
             editBTN.textContent = "Save";
         } else {
             editBTN.textContent = "Edit";
+            input.style.color = "rgb(97, 110, 131)";
             input.setAttribute("readonly", "");
         }
     })
-    
-    deleteBTN.addEventListener("click" , function(){
+
+    deleteBTN.addEventListener("click", function () {
         input.remove();
         editBTN.remove();
         deleteBTN.remove();
         doneBTN.remove();
+        div.remove();
+        buttons_div.remove();
+        alert("Are You Sure you want to delete the task?");
     })
-    
-    doneBTN.addEventListener("click" , function(){
-        input.style.backgroundColor="rgb(151, 240, 151)";
+
+    doneBTN.addEventListener("click", function () {
+        input.style.backgroundColor = "rgb(151, 240, 151)";
         input.style.color = "black";
         setTimeout(() => {
             input.remove();
             editBTN.remove();
             deleteBTN.remove();
             doneBTN.remove();
+            div.remove();
+            buttons_div.remove();
+            counter++;
+            tasksDone.textContent=counter;
         }, 1000);
     })
 
